3a17f393f41d37d84da1a3273efded9ca62835c5
jamesjavanyc` <1931736857@qq.com>
2023-02-19 23:01:25 -0500

    dep debug

--- src/controller/authentication.ts
+++ src/controller/authentication.ts
@@ -1,6 +1,9 @@
 import {Request, Response} from "express";
 
 const oauthLogin = (req:Request, res:Response):void =>{
-  res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env["GITHUB_OAUTH_ID "]}&redirect_uri=https://daycare.onrender.com/oauth-callback&scope=user:email`
-)
+  res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_OAUTH_ID}&redirect_uri=https://daycare.onrender.com/oauth-callback&scope=user:email`)
+}
+
+const oauthCallback = (req:Request, res:Response):void => {
+  console.log(res)
 }
